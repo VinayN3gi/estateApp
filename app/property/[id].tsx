6dc1541e167f0d087df40138e6cce3402e95286a
Vinay Negi <vinegi004@gmail.com>
2025-04-09 14:35:32 +0530

    Inital Commit

---
+++ app/property/[id].tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const Property = () => {
+
+
+    const {id}=useLocalSearchParams()
+    return (
+        <View>
+            <Text> Property {id}</Text>
+        </View>
+    )
+}
