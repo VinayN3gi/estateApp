01b8d00634214d25f2028e47654e5849d73071ca
Vinay Negi <vinegi004@gmail.com>
2025-04-12 22:02:14 +0530

    Add bottom navigation bar

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useAuth } from '@/context/AuthContext'
 
@@ -6,9 +6,13 @@ export default function home() {
   const {user}=useAuth();
 
   return (
-    <View>
-      <Text>{user?.email}</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-white'>
+      <ScrollView contentContainerStyle={{flexGrow:1}} className='px-6 py-12'>
+        <Text>
+          Home
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,15 @@
-import {StyleSheet, Text, View} from 'react-native'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+        <SafeAreaView className='flex-1 bg-white'>
+            <ScrollView contentContainerStyle={{flexGrow:1}} className='px-6 py-12'>
+                <Text>
+                    Profile
+                </Text>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 export default Profile
