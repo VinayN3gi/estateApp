4c83ffef968baaa634d0a443e908dc233bec6d03
Vinay Negi <vinegi004@gmail.com>
2025-04-13 09:03:34 +0530

    Add searchbar

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,17 +1,23 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useAuth } from '@/context/AuthContext'
+import Search from '@/components/Search';
 
 export default function home() {
   const {user}=useAuth();
 
   return (
-    <SafeAreaView className='flex-1 bg-white'>
-      <ScrollView contentContainerStyle={{flexGrow:1}} className='px-6 py-12'>
-        <Text>
-          Home
-        </Text>
-      </ScrollView>
+    <SafeAreaView className='h-full bg-white px-6'>
+      <Search/>
+      
+      <View className='my-5'>
+        <View className='flex flex-row items-center justify-between'>
+          <Text className='text-xl font-rubik-bol text-black-300'>Featured</Text>
+          <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+        </View>
+      </View>
+
+      
     </SafeAreaView>
   )
 }
